02f3f85f8ff67dd004a9fab0d64928944992f97b
Eduardo Souza <denis.souzadeoliveira05@gmail.com>
2024-11-21 22:32:09 -0300

    Compressão de imagens com sharp

--- image-compressor/backend/src/RabbitMQ/Consumer/CompressFile.ts
+++ image-compressor/backend/src/RabbitMQ/Consumer/CompressFile.ts
@@ -1,4 +1,6 @@
 import * as amqp from 'amqplib/callback_api';
+// import { Jimp } from 'jimp';
+import sharp from 'sharp';
 
 amqp.connect('amqp://localhost',function(error0: Error | null, connection: amqp.Connection):void{
     if(error0) throw error0;
@@ -16,7 +18,8 @@ amqp.connect('amqp://localhost',function(error0: Error | null, connection: amqp.
         channel.consume(queue, function(msg:any){
             const imagemToString = msg.content.toString();
             const imageToJson = JSON.parse(imagemToString)
-            console.log(imageToJson);
+
+            CompressImagem(imageToJson);
         },
         {
             noAck: true
@@ -27,7 +30,32 @@ amqp.connect('amqp://localhost',function(error0: Error | null, connection: amqp.
 })
 
 
-// function CompressImagem(imagem){
-//     // Implementar a lógica para compressão da imagem
+function CompressImagem(imageToJson:any){
+    // Implementar a lógica para compressão da imagem
+    
+    //Converte a string para base64 encoded
+    const inputFile = Buffer.from(imageToJson.buffer, 'base64');
+    // console.log(imageToJson);
+    const outputFile = `${imageToJson.originalname}_compressed`;
+    const compress_quality = 70;
 
-// }
+    if(imageToJson.mimetype === 'image/jpeg'){
+        // Compressão para JPEG
+        sharp(inputFile)
+        .jpeg({ quality: compress_quality }) // Ajusta a qualidade
+        .toFile('output.jpg') // Salva o arquivo comprimido
+        .then(() => console.log('Imagem comprimida com sucesso!'))
+        .catch(err => console.error(err));
+
+    }else if(imageToJson.mimetype === 'image/png'){
+        // Compressão para PNG
+        sharp(inputFile)
+        .png({ compressionLevel: 1 }) // Nível de compressão (0 a 9)
+        .toFile('output.png')
+        .then(() => console.log('Imagem comprimida com sucesso!'))
+        .catch(err => console.error(err));
+
+    }
+
+
+}
